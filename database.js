6aca653c5a0de6a2a3fbf3dc5cd54d8d7c75b27d
nattapong <nattapong_h@cmu.ac.th>
2018-12-11 10:39:27 +0700

    Add husky

--- database.js
+++ database.js
@@ -19,7 +19,7 @@ const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error'));
 db.once('open', function callback() {
-              console.log('Connection with database succeeded.');
+                   console.log('Connection with database succeeded.');
 });
 
 exports.db = db;
